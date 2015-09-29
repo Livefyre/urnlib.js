54bf30abba778a44134b4710a9a8ef4ae9fd334e
Jonathan Klaassen <jaklaassen@gmail.com>
2015-09-29 14:42:40 -0700

    Whitespace.

--- index.js
+++ index.js
@@ -1,133 +1,134 @@
-module.exports = (function(app) {
-    var URN = function(parts) {
-        this.parts = parts || [];
-    };
+module.exports = (function (app) {
+  var URN = function (parts) {
+    this.parts = parts || [];
+  };
 
-    URN.prototype.toString = function(excludeNs) {
-        var stringParts = [];
-        if (!excludeNs) {
-            stringParts = ['urn', 'livefyre'];
-        }
+  URN.prototype.toString = function (excludeNs) {
+    var stringParts = [];
+    if (!excludeNs) {
+      stringParts = ['urn', 'livefyre'];
+    }
 
-        for (var i = 0; i < this.parts.length; i++) {
-            var part = this.parts[i];
-            if (part[0]) {
-                stringParts.push(part[0] + '=' + part[1]);
-            } else {
-                stringParts.push(part[1]);
-            }
-        }
-        return stringParts.join(':');
-    };
+    for (var i = 0; i < this.parts.length; i++) {
+      var part = this.parts[i];
+      if (part[0]) {
+        stringParts.push(part[0] + '=' + part[1]);
+      } else {
+        stringParts.push(part[1]);
+      }
+    }
+    return stringParts.join(':');
+  };
 
-    URN.prototype.copy = function(type, id) {
-        var parts = [];
-        for (var i = 0; i < this.parts.length; i++) {
-            parts.push(Array.prototype.slice.call(this.parts[i]));
-        }
-        return new URN(parts);
-    };
+  URN.prototype.copy = function (type, id) {
+    var parts = [];
+    for (var i = 0; i < this.parts.length; i++) {
+      parts.push(Array.prototype.slice.call(this.parts[i]));
+    }
+    return new URN(parts);
+  };
 
-    URN.prototype.add = function(type, id) {
-        this.parts.push([type, id]);
-        return this;
-    };
+  URN.prototype.add = function (type, id) {
+    this.parts.push([type, id]);
+    return this;
+  };
 
-    URN.prototype.getParent = function(nLevels) {
-        if (nLevels === undefined) {
-            nLevels = 1;
-        }
-        return new URN(this.parts.slice(0, this.parts.length - nLevels));
-    };
+  URN.prototype.getParent = function (nLevels) {
+    if (nLevels === undefined) {
+      nLevels = 1;
+    }
+    return new URN(this.parts.slice(0, this.parts.length - nLevels));
+  };
 
-    URN.prototype.getParentByName = function(name) {
-        for (var i = 1; i <= this.parts.length; i++) {
-            if (this.parts[this.parts.length - i][0] == name) {
-                return this.getParent(i - 1);
-            }
-        }
-    };
+  URN.prototype.getParentByName = function (name) {
+    for (var i = 1; i <= this.parts.length; i++) {
 
-    URN.prototype.getRoot = function() {
-        return new URN([this.parts[0]]);
-    };
+      if (this.parts[this.parts.length - i][0] == name) {
+        return this.getParent(i - 1);
+      }
+    }
+  };
 
-    URN.prototype.getType = function() {
-        return this.parts[this.parts.length - 1][0]
-    };
+  URN.prototype.getRoot = function () {
+    return new URN([this.parts[0]]);
+  };
 
-    URN.prototype.getId = function() {
-        return this.parts[this.parts.length - 1][1];
-    };
+  URN.prototype.getType = function () {
+    return this.parts[this.parts.length - 1][0]
+  };
 
-    URN.prototype.contains = function(urnOrUrnString) {
-        var urn = urnOrUrnString;
-        if (typeof urn === 'string') {
-            urn = parse(urn);
-        }
+  URN.prototype.getId = function () {
+    return this.parts[this.parts.length - 1][1];
+  };
 
-        var len = urn.parts.length;
+  URN.prototype.contains = function (urnOrUrnString) {
+    var urn = urnOrUrnString;
+    if (typeof urn === 'string') {
+      urn = parse(urn);
+    }
 
-        // Every resource of this object should be part of input urn.
-        return this.parts.every(function(part, idx) {
-            if (idx >= len) {
-                return false;
-            }
+    var len = urn.parts.length;
 
-            if (part.length !== urn.parts[idx].length) {
-                return false;
-            }
+    // Every resource of this object should be part of input urn.
+    return this.parts.every(function (part, idx) {
+      if (idx >= len) {
+        return false;
+      }
 
-            return part.every(function(u, i) {
-                return u === urn.parts[idx][i];
-            });
-        });
-    };
+      if (part.length !== urn.parts[idx].length) {
+        return false;
+      }
 
-    var parse = function(urn) {
-        var parts = urn.split(':');
-        var result = [];
+      return part.every(function (u, i) {
+        return u === urn.parts[idx][i];
+      });
+    });
+  };
 
-        if (parts[0] == 'urn') {
-            parts = parts.slice(2);
-        }
-        for (var i = 0; i < parts.length; i++) {
-            var compParts = parts[i].split('=');
-            if (compParts.length == 2) {
-                result.push(compParts);
-            } else {
-                result.push([null, compParts[0]]);
-            }
+  var parse = function (urn) {
+    var parts = urn.split(':');
+    var result = [];
 
-        }
-        return new URN(result);
-    };
+    if (parts[0] == 'urn') {
+      parts = parts.slice(2);
+    }
+    for (var i = 0; i < parts.length; i++) {
+      var compParts = parts[i].split('=');
+      if (compParts.length == 2) {
+        result.push(compParts);
+      } else {
+        result.push([null, compParts[0]]);
+      }
 
-    var fromUrlPath = function(path, networkParam) {
-        if (path.substr(0, 1) == '/') {
-            path = path.slice(1);
-        }
-        var parts = [];
-        var part = [];
-        if (networkParam) {
-            part.push(null);
-        }
-        path.split("/").forEach(function(elem) {
-            part.push(elem);
-            if (part.length == 2) {
-                parts.push(part);
-                part = [];
-            }
-        });
-        if (part.length > 0) {
-            parts.push(part);
-        }
-        return new URN(parts);
-    };
+    }
+    return new URN(result);
+  };
 
-    return {
-        parse: parse,
-        fromUrlPath: fromUrlPath,
-        URN: URN
-    };
+  var fromUrlPath = function (path, networkParam) {
+    if (path.substr(0, 1) == '/') {
+      path = path.slice(1);
+    }
+    var parts = [];
+    var part = [];
+    if (networkParam) {
+      part.push(null);
+    }
+    path.split("/").forEach(function (elem) {
+      part.push(elem);
+      if (part.length == 2) {
+        parts.push(part);
+        part = [];
+      }
+    });
+    if (part.length > 0) {
+      parts.push(part);
+    }
+    return new URN(parts);
+  };
+
+  return {
+    parse: parse,
+    fromUrlPath: fromUrlPath,
+    URN: URN
+  };
 })();
--- test.js
+++ test.js
@@ -1,100 +1,100 @@
 var expect = require('chai').expect;
 var urnlib = require('./index.js');
 
-describe('urn.test.js', function() {
-    var urnStr = 'urn:livefyre:livefyre.com:site=123456:collection=100000';
-    var urn = urnlib.parse(urnStr);
+describe('urn.test.js', function () {
+  var urnStr = 'urn:livefyre:livefyre.com:site=123456:collection=100000';
+  var urn = urnlib.parse(urnStr);
 
-    describe('URN.parse', function() {
-        it('Successfully parses a URN', function(done) {
-            expect(urn.parts).to.eql([
-                [null, 'livefyre.com'],
-                ['site', '123456'],
-                ['collection', '100000']
-            ]);
-            expect(urn.toString()).to.equal(urnStr);
-            done();
-        });
+  describe('URN.parse', function () {
+    it('Successfully parses a URN', function (done) {
+      expect(urn.parts).to.eql([
+        [null, 'livefyre.com'],
+        ['site', '123456'],
+        ['collection', '100000']
+      ]);
+      expect(urn.toString()).to.equal(urnStr);
+      done();
     });
-    describe('URN.getParent', function() {
-        it('Successfully get a URN\'s parent(s)', function(done) {
-            expect(urn.getParent(0).toString()).to.equal(urnStr);
-            expect(urn.getParent(1).toString()).to.equal('urn:livefyre:livefyre.com:site=123456');
-            expect(urn.getParent(2).toString()).to.equal('urn:livefyre:livefyre.com');
-            done();
-        });
+  });
+  describe('URN.getParent', function () {
+    it('Successfully get a URN\'s parent(s)', function (done) {
+      expect(urn.getParent(0).toString()).to.equal(urnStr);
+      expect(urn.getParent(1).toString()).to.equal('urn:livefyre:livefyre.com:site=123456');
+      expect(urn.getParent(2).toString()).to.equal('urn:livefyre:livefyre.com');
+      done();
     });
-    describe('URN.getParentByName', function() {
-        it('Successfully get a URN\'s parent(s) by name', function(done) {
-            expect(urn.getParentByName('collection').toString()).to.equal(urnStr);
-            expect(urn.getParentByName('site').toString()).to.equal('urn:livefyre:livefyre.com:site=123456');
-            done();
-        });
+  });
+  describe('URN.getParentByName', function () {
+    it('Successfully get a URN\'s parent(s) by name', function (done) {
+      expect(urn.getParentByName('collection').toString()).to.equal(urnStr);
+      expect(urn.getParentByName('site').toString()).to.equal('urn:livefyre:livefyre.com:site=123456');
+      done();
     });
-    describe('URN.root', function() {
-        it('Successfully get a URN\'s root', function(done) {
-            expect(urn.getRoot().toString()).to.equal('urn:livefyre:livefyre.com');
-            done();
-        });
+  });
+  describe('URN.root', function () {
+    it('Successfully get a URN\'s root', function (done) {
+      expect(urn.getRoot().toString()).to.equal('urn:livefyre:livefyre.com');
+      done();
     });
-    describe('URN.getType', function() {
-        it('Successfully get a URN\'s type', function(done) {
-            expect(urn.getType()).to.equal('collection');
-            done();
-        });
+  });
+  describe('URN.getType', function () {
+    it('Successfully get a URN\'s type', function (done) {
+      expect(urn.getType()).to.equal('collection');
+      done();
     });
-    describe('URN.getId', function() {
-        it('Successfully get a URN\'s ID', function(done) {
-            expect(urn.getId()).to.equal('100000');
-            // This should work for null-type parts too.
-            expect(urnlib.parse('urn:livefyre:livefyre.com').getId()).to.equal('livefyre.com');
-            done();
-        });
+  });
+  describe('URN.getId', function () {
+    it('Successfully get a URN\'s ID', function (done) {
+      expect(urn.getId()).to.equal('100000');
+      // This should work for null-type parts too.
+      expect(urnlib.parse('urn:livefyre:livefyre.com').getId()).to.equal('livefyre.com');
+      done();
     });
-    describe('URN.add', function() {
-        it('Successfully add an element to a URN', function(done) {
-            var urn = new urnlib.URN();
-            urn.add('thingType', 'thingId');
-            expect(urn.toString()).to.equal('urn:livefyre:thingType=thingId');
-            done();
-        });
+  });
+  describe('URN.add', function () {
+    it('Successfully add an element to a URN', function (done) {
+      var urn = new urnlib.URN();
+      urn.add('thingType', 'thingId');
+      expect(urn.toString()).to.equal('urn:livefyre:thingType=thingId');
+      done();
     });
-    describe('URN.contains', function() {
-        it('returns true if urn had all resources of input urn', function(done) {
+  });
+  describe('URN.contains', function () {
+    it('returns true if urn had all resources of input urn', function (done) {
 
-            var urnAncestor = urnlib.parse('urn:livefyre:livefyre.com');
-            expect(urnAncestor.contains(urn)).to.equal(true);
+      var urnAncestor = urnlib.parse('urn:livefyre:livefyre.com');
+      expect(urnAncestor.contains(urn)).to.equal(true);
 
-            urnAncestor = urnlib.parse('urn:livefyre:livefyre.com:site=123456');
-            expect(urnAncestor.contains(urn)).to.equal(true);
+      urnAncestor = urnlib.parse('urn:livefyre:livefyre.com:site=123456');
+      expect(urnAncestor.contains(urn)).to.equal(true);
 
-            urnAncestor = urnlib.parse(
-                'urn:livefyre:livefyre.com:site=123456:collection=100000');
-            expect(urnAncestor.contains(urn)).to.equal(true);
+      urnAncestor = urnlib.parse(
+        'urn:livefyre:livefyre.com:site=123456:collection=100000');
+      expect(urnAncestor.contains(urn)).to.equal(true);
 
-            urnAncestor = urnlib.parse('urn:livefyre:livefyre.com:site=666');
-            expect(urnAncestor.contains(urn)).to.equal(false);
+      urnAncestor = urnlib.parse('urn:livefyre:livefyre.com:site=666');
+      expect(urnAncestor.contains(urn)).to.equal(false);
 
-            urnAncestor = urnlib.parse('urn:livefyre:example.com');
-            expect(urnAncestor.contains(urn)).to.equal(false);
+      urnAncestor = urnlib.parse('urn:livefyre:example.com');
+      expect(urnAncestor.contains(urn)).to.equal(false);
 
-            done();
-        });
-
-        it('should not break because urn is shorter', function(done) {
-            var shortUrn = urnlib.parse('urn:livefyre:livefyre.com');
-            var longUrn = urnlib.parse('urn:livefyre:livefyre.com:site=666');
-
-            expect(longUrn.contains(shortUrn)).to.equal(false);
-
-            done();
-        });
+      done();
     });
-    describe('URN.fromUrlPath', function() {
-        it('should ignore starting slash', function() {
-            var urn = urnlib.fromUrlPath('/a/1');
-            expect(urn.getId()).to.equal('1');
-            expect(urn.getType()).to.equal('a');
-        });
+
+    it('should not break because urn is shorter', function (done) {
+      var shortUrn = urnlib.parse('urn:livefyre:livefyre.com');
+      var longUrn = urnlib.parse('urn:livefyre:livefyre.com:site=666');
+
+      expect(longUrn.contains(shortUrn)).to.equal(false);
+
+      done();
     });
+  });
+  describe('URN.fromUrlPath', function () {
+    it('should ignore starting slash', function () {
+      var urn = urnlib.fromUrlPath('/a/1');
+      expect(urn.getId()).to.equal('1');
+      expect(urn.getType()).to.equal('a');
+    });
+  });
 });
